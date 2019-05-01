4b702c43c0fff5ede507bd051b1c0a255d281181
Ben Barantschik <bbarantschik@gmail.com>
2019-04-30 17:21:15 -0700

    implemented GAE

--- policy_gradients.py
+++ policy_gradients.py
@@ -7,7 +7,7 @@ import time
 import schedules
 
 class VanillaPolicy:
-    def __init__(self, model, value_model,
+    def __init__(self, model,
                  env_creator, lr_schedule,
                  min_observations_per_step,
                  log, gamma, fp_observations, render = False, render_mod = 16):
@@ -34,12 +34,12 @@ class VanillaPolicy:
                                               shape = [None] + list(dummy_env.observation_space.shape),
                                               name = "observation")
         if fp_observations:
-            obs_input = self.obs_placeholder
+            self.obs_input = self.obs_placeholder
         else:
-            obs_input = tf.cast(self.obs_placeholder, tf.float32)
-        self.net_op = model(obs_input,
+            self.obs_input = tf.cast(self.obs_placeholder, tf.float32)
+        self.net_op = model(self.obs_input,
                             out_size = dummy_env.action_space.n,
-                            scope = "mlp")
+                            scope = "policy_net")
 
         self.action_placeholder = tf.placeholder(tf.int32, shape = [None],
                                               name = "action")
@@ -57,20 +57,47 @@ class VanillaPolicy:
         self.approx_entropy_op = -tf.reduce_mean(self.logprob_op, name = "approximate_entropy")
         self.approx_kl_divergence_op = -tf.reduce_mean(self.logprob_sample_placeholder - self.logprob_op)
 
-        self.policy_value_op = tf.reduce_sum(self.logprob_op * self.adv_placeholder, name = "policy_value")
+        self.policy_value_op = tf.reduce_mean(self.logprob_op * self.adv_placeholder, name = "policy_value")
         self.update_op = tf.train.AdamOptimizer(self.lr_placeholder).minimize(-self.policy_value_op)
 
+
+    def initialize_variables(self):
         tf_config = tf.ConfigProto()
         tf_config.gpu_options.allow_growth = True
         self.session = tf.Session(config = tf_config)
         tf.global_variables_initializer().run(session = self.session)
 
+
+    def _initialize_sample_path_dict(self):
+        return {
+            'observations': [],
+            'rewards': [],
+            'actions': [],
+            'logprobs': [],
+            'info': {}
+        }
+
+    def _step_once(self, path, env, obs):
+        path['observations'].append(obs)
+        action, logprob = self.session.run(
+            [self.sample_op, self.logprob_sample_op],
+            feed_dict = {
+                self.obs_placeholder: obs[None]
+            }
+        )
+        action = action[0]
+        path['actions'].append(action)
+        path['logprobs'].append(logprob[0])
+
+        obs, reward, done, info = env.step(action)
+        self.env_creator.update(done, info)
+        path['rewards'].append(reward)
+
+        return obs, reward, done, info
+
     def sample_trajectory(self):
         env = self.env_creator.new_env()
-        observations = []
-        rewards = []
-        actions = []
-        logprobs = []
+        path = self._initialize_sample_path_dict()
         obs = env.reset()
 
         # This counts how many actions were in directions without nodes
@@ -80,20 +107,7 @@ class VanillaPolicy:
             if self.render and self.n_episodes % self.render_mod == 0:
                 env.render()
 
-            observations.append(obs)
-            action, logprob = self.session.run(
-                [self.sample_op, self.logprob_sample_op],
-                feed_dict = {
-                    self.obs_placeholder: obs[None]
-                }
-            )
-            action = action[0]
-            actions.append(action)
-            logprobs.append(logprob[0])
-
-            obs, reward, done, info = env.step(action)
-            self.env_creator.update(done, info)
-            rewards.append(reward)
+            obs, reward, done, info = self._step_once(path, env, obs)
 
             # if we are doing CartPole-v0, we just ignore this
             if 'correct_direction' in info and not info['correct_direction']:
@@ -107,24 +121,38 @@ class VanillaPolicy:
             time.sleep(1) # so that we have time to see the end
             env.close()
 
-        info = {
-            'n_useless_actions': n_useless_actions,
-            'n_steps': len(observations)
-        }
+        path['info']['n_useless_actions'] = n_useless_actions,
+        path['info']['n_steps'] =  len(path['observations'])
 
         self.n_episodes += 1
-        return observations, rewards, actions, logprobs, info
+        return path
 
+    def _initialize_path_dict(self):
+        return {
+            'observations': [],
+            'advantages': [],
+            'actions': [],
+            'reward_totals': [],
+            'logprobs': [],
+            'info': []
+        }
+
+    def _calculate_discounted_to_go(self, values, coef):
+        accumulator = 0.0
+        discounted = []
+        for value in values[::-1]:
+            accumulator *= coef
+            accumulator += value
+            discounted.append(accumulator)
+        return discounted[::-1]
+
+    def _calculate_advantages(self, path, path_i):
+        path['advantages'].extend(self._calculate_discounted_to_go(path_i['rewards'], self.gamma))
 
     def sample_trajectories(self):
-        observations = []
-        advantages = []
-        actions = []
-        reward_totals = []
-        logprobs = []
-        info = []
-        while len(observations) < self.min_observations_per_step:
-            observations_i, rewards_i, actions_i, logprobs_i, info_i = self.sample_trajectory()
+        path = self._initialize_path_dict()
+        while len(path['observations']) < self.min_observations_per_step:
+            path_i = self.sample_trajectory()
 
             # We don't want to mix episodes where the environment is of varying difficulties.
             # Since environments will not frequently increase in difficulty, I think it's 
@@ -133,54 +161,48 @@ class VanillaPolicy:
                 print("Environment type changing!")
                 return self.sample_trajectories()
 
-            observations.extend(observations_i)
-            reward_totals.append(sum(rewards_i))
-            actions.extend(actions_i)
-            logprobs.extend(logprobs_i)
-            info.append(info_i)
+            path['observations'].extend(path_i['observations'])
+            path['actions'].extend(path_i['actions'])
+            path['logprobs'].extend(path_i['logprobs'])
+
+            path['reward_totals'].append(sum(path_i['rewards']))
+            path['info'].append(path_i['info'])
 
             # cummulative rewards
-            cum_reward = 0.0
-            advantages_i = []
-            for reward in rewards_i[::-1]:
-                cum_reward *= self.gamma
-                cum_reward += reward
-                advantages_i.append(cum_reward)
-            advantages.extend(advantages_i[::-1])
+            self._calculate_advantages(path, path_i)
         
-        observations = np.array(observations)
-        advantages = np.array(advantages)
+        path['observations'] = np.array(path['observations'])
+        path['advantages'] = np.array(path['advantages'])
         
         # normalizing advantages
-        advantages -= np.mean(advantages)
-        advantages /= np.std(advantages)
+        path['advantages'] -= np.mean(path['advantages'])
+        path['advantages'] /= np.std(path['advantages'])
 
-        actions = np.array(actions)
+        path['reward_totals'] = np.array(path['reward_totals'])
+        path['actions'] = np.array(path['actions'])
 
-        return observations, advantages, actions, reward_totals, logprobs, info
+        return path
         
 
     def optimize(self, total_steps):
         steps = 0
 
         while steps < total_steps:
-            observations, advantages, actions, reward_totals, logprobs, info = self.sample_trajectories()
+            path = self.sample_trajectories()
             _, approximate_entropy = self.session.run(
                 [self.update_op, self.approx_entropy_op],
                 feed_dict = {
-                    self.obs_placeholder: observations,
-                    self.action_placeholder: actions,
-                    self.adv_placeholder: advantages,
+                    self.obs_placeholder: path['observations'],
+                    self.action_placeholder: path['actions'],
+                    self.adv_placeholder: path['advantages'],
                     self.lr_placeholder: self.lr_schedule(steps)
                 }
             )
-            steps += observations.shape[0]
-
-            reward_totals = np.array(reward_totals)
+            steps += path['observations'].shape[0]
 
             log_data = {
-                'average reward': np.mean(reward_totals),
-                'std of reward': np.std(reward_totals),
+                'average reward': np.mean(path['reward_totals']),
+                'std of reward': np.std(path['reward_totals']),
                 'approximate action entropy': approximate_entropy,
                 'simulation steps': steps
             }
@@ -195,19 +217,16 @@ if __name__ == '__main__':
         model = (lambda *args, **varargs: models.mlp(n_layers = 2,
                                                      hidden_size = 64,
                                                      *args, **varargs)),
-        value_model = (lambda *args, **varargs: tf.squeeze(models.mlp(n_layers = 2,
-                                                           hidden_size = 64,
-                                                           out_size = 1,
-                                                           *args, **varargs), axis = 1)),
         # env_creator = schedules.ExploreCreatorSchedule(is_tree = False, history_size = 2),
         env_creator = schedules.CartPoleDummySchedule(),
         lr_schedule = lambda t: 5e-3,
         min_observations_per_step = 1000,
         log = log,
         gamma = 1.0,
-        fp_observations = False,
+        fp_observations = True,
         render = False,
         render_mod = 128
     )
+    vp.initialize_variables()
     vp.optimize(2000000)
     log.close()
---
+++ policy_gradients_gae.py
@@ -0,0 +1,116 @@
+from policy_gradients import VanillaPolicy
+import models
+import tensorflow as tf
+import numpy as np
+import loggy
+import schedules
+
+# Implements GAE-Lambda with value estimator. This is based off of
+# spinningup's implementation
+class VanillaPolicyGAE(VanillaPolicy):
+    def __init__(self, value_model, value_lr_schedule, lambda_gae, *args, **varargs):
+        super().__init__(*args, **varargs)
+
+        self.value_lr_schedule = value_lr_schedule
+        self.lambda_gae = lambda_gae
+
+        self.value_net_op = value_model(self.obs_input, scope = "value_net")
+        self.return_placeholder = tf.placeholder(tf.float32, shape = [None], name = "returns")
+        self.value_lr_placeholder = tf.placeholder(tf.float32, shape = [], name = "value_learning_rate")
+
+        # TODO: replace square difference with Huber loss?
+        self.value_loss = tf.reduce_mean((self.value_net_op - self.return_placeholder) ** 2)
+        self.update_value_op = tf.train.AdamOptimizer(self.value_lr_placeholder).minimize(self.value_loss)
+
+    def _initialize_sample_path_dict(self):
+        path = super()._initialize_sample_path_dict()
+        path['values'] = []
+        return path
+
+    def _step_once(self, path, env, obs):
+        path['observations'].append(obs)
+        action, logprob, value = self.session.run(
+            [self.sample_op, self.logprob_sample_op, self.value_net_op],
+            feed_dict = {
+                self.obs_placeholder: obs[None]
+            }
+        )
+        action = action[0]
+        path['actions'].append(action)
+        path['logprobs'].append(logprob[0])
+        path['values'].append(value[0])
+
+        obs, reward, done, info = env.step(action)
+        self.env_creator.update(done, info)
+        path['rewards'].append(reward)
+
+        return obs, reward, done, info
+
+    def _initialize_path_dict(self):
+        path = super()._initialize_path_dict()
+        path['returns'] = []
+        return path
+
+    def _calculate_advantages(self, path, path_i):
+        path['returns'].extend(self._calculate_discounted_to_go(path_i['rewards'], self.gamma))
+
+        # This is the GAE calculation, slightly modified from spinningup
+        shifted_values = np.append(path_i['values'], 0)[1:]
+        deltas = path_i['rewards'] + self.gamma * shifted_values - path_i['values']
+        path['advantages'].extend(self._calculate_discounted_to_go(deltas, self.gamma * self.lambda_gae))
+
+    def optimize(self, total_steps):
+        steps = 0
+
+        while steps < total_steps:
+            path = self.sample_trajectories()
+            _, _, approximate_entropy = self.session.run(
+                [self.update_op, self.update_value_op, self.approx_entropy_op],
+                feed_dict = {
+                    self.obs_placeholder: path['observations'],
+                    self.action_placeholder: path['actions'],
+                    self.adv_placeholder: path['advantages'],
+                    self.return_placeholder: path['returns'],
+                    self.lr_placeholder: self.lr_schedule(steps),
+                    self.value_lr_placeholder: self.value_lr_schedule(steps)
+                }
+            )
+            steps += path['observations'].shape[0]
+
+            log_data = {
+                'average reward': np.mean(path['reward_totals']),
+                'std of reward': np.std(path['reward_totals']),
+                'approximate action entropy': approximate_entropy,
+                'simulation steps': steps
+            }
+            self.env_creator.add_logging_data(log_data)
+
+            self.log.step(log_data)
+            self.log.print_step()
+
+
+if __name__ == '__main__':
+    log = loggy.Log("test")
+    vpgae = VanillaPolicyGAE(
+        model = (lambda *args, **varargs: models.mlp(n_layers = 2,
+                                                     hidden_size = 64,
+                                                     *args, **varargs)),
+        value_model = (lambda *args, **varargs: tf.squeeze(models.mlp(n_layers = 2,
+                                                                hidden_size = 64,
+                                                                out_size = 1,
+                                                                *args, **varargs), axis = 1)),
+        # env_creator = schedules.ExploreCreatorSchedule(is_tree = False, history_size = 2),
+        env_creator = schedules.CartPoleDummySchedule(),
+        lr_schedule = (lambda t: 5e-3),
+        value_lr_schedule = (lambda t: 5e-3),
+        lambda_gae = .95,
+        min_observations_per_step = 1000,
+        log = log,
+        gamma = 1.0,
+        fp_observations = True,
+        render = False,
+        render_mod = 128
+    )
+    vpgae.initialize_variables()
+    vpgae.optimize(2000000)
+    log.close()
